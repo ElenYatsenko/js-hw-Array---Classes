cf384e0ff148b4c897c65c3a4abb876c825c7aae
Elen <15el.shev@gmail.com>
2022-05-15 14:26:21 +0300

    Complete the first part of the task

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
 "use strict";
 
-// concat
-// Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
-const arrayA = ["1", "2", "3"];
-const arrayB = ["4", "5", "6"];
-const arrayC = arrayA.concat(arrayB);
-console.log(arrayC);
+const arrayFirst = [1, 2, 3];
+const arrayTwo = [4, 5, 6];
+const arrayConcat = arrayFirst.concat(arrayTwo);
+console.log(arrayConcat);
 
-// reverse
-// Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
-const arrayD = ["1", "2", "3"];
-console.log(arrayD.reverse());
+const arrayReverse = [1, 2, 3];
+console.log(arrayReverse.reverse());
 
-// push, unshift
-// Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.
-// Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
-/////////push/////////
-const arrayE = ["1", "2", "3"];
-arrayE.push("4", "5", "6");
+const arrayPush = [1, 2, 3];
+arrayPush.push(4, 5, 6);
+console.log(arrayPush);
+
+const arrayUnshift = [1, 2, 3];
+arrayUnshift.unshift(4, 5, 6);
+console.log(arrayUnshift);
+
+const arrayShift = ["js", "css", "html"];
+console.log(arrayShift[0]);
+arrayShift.shift();
+console.log(arrayShift);
+
+const arrayPop = ["js", "css", "html"];
+console.log(arrayPop[arrayPop.length - 1]);
+arrayPop.pop();
+console.log(arrayPop);
+
+const arrayA = [1, 2, 3, 4, 5];
+const arrayB = arrayA.slice(0, 3);
+console.log(arrayB);
+
+const arrayC = [1, 2, 3, 4, 5];
+const arrayD = arrayC.slice(3);
+console.log(arrayD);
+
+const arrayE = [1, 2, 3, 4, 5];
+arrayE.splice(0, 3, 1);
 console.log(arrayE);
-/////////unshift/////////
-const arrayF = ["1", "2", "3"];
-arrayF.unshift("4", "5", "6");
-console.log(arrayF);
 
-// shift, pop
-// Дан массив ['js', 'css', ‘html']. Выведите на экран первый элемент и удалите его.
-// Дан массив ['js', 'css', ‘html']. Выведите на экран последний элемент и удалите его.
-/////////shift/////////
-const arrayG = ["js", "css", "html"];
-console.log(arrayG[0]);
-arrayG.shift();
+const arrayF = [1, 2, 3, 4, 5];
+const arrayG = arrayF.splice(1, 3);
 console.log(arrayG);
-/////////pop/////////
-const arrayH = ["js", "css", "html"];
-console.log(arrayH[arrayH.length - 1]);
-arrayH.pop();
+
+const arrayH = [1, 2, 3, 4, 5];
+arrayH.splice(3, 0, "a", "b", "c");
 console.log(arrayH);
 
-// slice
-// Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [1, 2, 3].
-// Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [4, 5].
-// splice
-//  Дан массив [1, 2, 3, 4, 5]. С помощью метода splice преобразуйте массив в [1, 4, 5].
-// Дан массив [1, 2, 3, 4, 5]. С помощью метода splice запишите в новый массив элементы [2, 3, 4].
-//  Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 2, 3, 'a', 'b', 'c', 4, 5].
-//  Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+const arrayI = [1, 2, 3, 4, 5];
+arrayI.splice(1, 0, "a", "b");
+arrayI.splice(6, 0, "c");
+arrayI.splice(8, 0, "e");
+console.log(arrayI);
 
-// sort
-// Дан массив [3, 4, 1, 2, 7]. Отсортируйте его.
+const arrayJ = [3, 4, 1, 2, 7];
+arrayJ.sort();
+console.log(arrayJ);
 
 // Во всех задания ниже сделать функцию!!!
 // В комментариях к коду описать задание.
-// =========================================================
+// =========================================================//
 // Необходимо создать функцию hasElem, которая параметрами будет принимать массив и строку, и возвращать true, если строка есть в массиве, и false - если нет
 // Дан массив с числами. Проверьте, что в этом массиве есть указанное число. Если есть - вернуть true, а если нет - вернуть false.
 // Дан массив с числами. Проверьте, есть ли в нем два одинаковых числа подряд. Если есть -  вернуть true[2,2,1], а если нет - вернуть false[1,2,1]
